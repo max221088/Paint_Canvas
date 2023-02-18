9a52941824add948546702dc4319e3d997cdbd7a
max221088 <117590988+max221088@users.noreply.github.com>
2023-02-19 01:58:37 +0200

    Add figure

--- js/script.js
+++ js/script.js
@@ -1,23 +1,88 @@
-let $area = document.querySelector('.area');
-const $main = document.querySelector('.main');
-let $color = document.querySelector('.color');
-let $figure = document.querySelector('.figure');
-let ctx = $area.getContext('2d');
-let x = null;
-let y = null;
-let height = null;
-let width = null;
+// Vars section
 
-$area.addEventListener('mousedown', function(event){
-    x = event.pageX;
-    y = event.pageY;
-    console.log(x);
-    console.log(y);
-    let xx = $area.getBoundingClientRect();
-    console.log(xx);
-    console.log(xx.top);
-    ctx.fillStyle = $color.setAttribute("type", "color");
-    ctx.fillRect(x, y, 100, 100);
+const $area = document.querySelector('.area');
+const $main = document.querySelector('.main');
+const $color = document.querySelector('.color');
+const $linecolor = document.querySelector('.linecolor');
+const $figure = document.querySelector('.figure');
+const ctx = $area.getContext('2d');
+let color = 'black';
+let lineColor = 'black';
+let figure = 'Line';
+let begin = false;
+let coords = [x = 0, y = 0];
+let size = [w = 0, h = 0];
+let ScreenHeight = window.screen.availHeight - 50;
+let ScreenWidth = window.screen.availWidth;
+
+// Init section
+
+$area.width = ScreenWidth;
+$area.height = ScreenHeight;
+
+$color.addEventListener("input", function(e){
+    color = e.target.value;
 });
 
+$linecolor.addEventListener("input", function(e){
+    lineColor = e.target.value;
+});
+
+$figure.addEventListener("change", function(e){
+    figure = e.target.value;
+});
+
+// Action section
+
+$area.addEventListener('mousedown', function(e){
+    coords.x = e.pageX - this.offsetLeft;
+    coords.y = e.pageY - this.offsetTop;
+    ctx.fillStyle = color;
+    if (figure === 'square') {
+        begin = 'square';
+    }
+    if (figure === 'Line') {
+        begin = 'Line';
+        ctx.beginPath();
+        ctx.strokeStyle = lineColor;
+        ctx.moveTo(coords.x, coords.y);
+    }
+    if (figure === 'circle') {
+        begin = 'circle';
+        ctx.beginPath();
+        
+    }
+    
+});
+
+$area.addEventListener('mousemove', function(e){
+    if ((figure === 'square') && (begin === 'square')) {
+        size.w = e.pageX - coords.x - this.offsetLeft;
+        size.h = e.pageY - coords.y - this.offsetTop;
+        console.log(size);
+        ctx.fillRect(coords.x, coords.y, size.w, size.h);
+    }
+    if ((figure === 'Line') && (begin === 'Line')) {
+        ctx.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+        ctx.stroke();
+    }
+    if ((figure === 'circle') && (begin === 'circle')) {
+        size.w = e.pageX - coords.x - this.offsetLeft;
+        size.h = e.pageY - coords.y - this.offsetTop;
+        ctx.arc(coords.x, coords.y ,Math.abs(size.w) ,0,Math.PI*2,true);
+        ctx.strokeStyle = lineColor;
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = lineColor;
+        ctx.stroke();
+    }
+});
+
+$area.addEventListener('mouseup', function(e){
+    if (begin) {
+        begin = false;
+        ctx.closePath();
+    }
+});
 
